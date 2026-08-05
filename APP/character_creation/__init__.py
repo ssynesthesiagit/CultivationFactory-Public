76f615d8b91f191b1a50b7ee28793863c9a8f7b1
ssynesthesiagit <ssynesthesiagit@gmail.com>
2026-08-04 22:46:31 -0400

    Apply Stage1-first scratch compilation order

--- APP/character_creation/__init__.py
+++ APP/character_creation/__init__.py
@@ -1,3 +1,7 @@
 from .service import CharacterCreationExecutionService
+from .stage1_first_compile import compile_once_stage1_first
+
+
+CharacterCreationExecutionService._compile_once = compile_once_stage1_first
 
 __all__ = ["CharacterCreationExecutionService"]
